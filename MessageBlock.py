3cfedcf940a6384a97057f3da570d16a5fede7a6
m.bohlooli <bohlool82@gmail.com>
2024-05-25 20:43:28 +0330

    Initial Commit - Create Message class abstract class and array message block write method

---
+++ MessageBlock.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+from math import log2
+
+HAMMING_ENCODE = "hamming"
+PARITY_2D_ENCODE = "parity_2d"
+
+
+class MessageBlock(ABC):
+    @property
+    @abstractmethod
+    def encoding(self) -> str:
+        pass
+
+    @abstractmethod
+    def write(self, data: str, verbose: bool = False) -> None:
+        pass
+
+    @abstractmethod
+    def read(self, verbose: bool = False) -> str:
+        pass
+
+    @abstractmethod
+    def validate(self, verbose: bool = False) -> None:
+        pass
+
+
+class ArrayMessageBlock(MessageBlock):
+    def __init__(self):
+        self.message = [0 for _ in range(16)]
+
+    def write(self, data: str, verbose: bool = False) -> None:
+        if len(data) > 11:
+            raise ValueError("Data is too long")
+
+        while len(data) < 11:
+            data += '0'
+
+        data_reversed = list(map(int, data[::-1]))
+
+        for index, _ in enumerate(self.message):
+            if index == 0 or log2(index).is_integer():
+                continue
+
+            self.message[index] = data_reversed.pop()
+
+        if verbose:
+            print(f'encoded data: {self.message}')
+
+    def read(self, verbose: bool = False) -> str:
+        pass
+
+    def validate(self, verbose: bool = False) -> None:
+        pass
+
+    def encoding(self) -> str:
+        return HAMMING_ENCODE
+
+
+class MatrixMessageBlock(MessageBlock):
+    def write(self, data: str, verbose: bool = False) -> None:
+        pass
+
+    def read(self, verbose: bool = False) -> str:
+        pass
+
+    def validate(self, verbose: bool = False) -> None:
+        pass
+
+    def encoding(self) -> str:
+        return PARITY_2D_ENCODE
---
+++ main.py
@@ -0,0 +1,6 @@
+from MessageBlock import *
+
+if __name__ == '__main__':
+    message_block: MessageBlock = ArrayMessageBlock()
+    message_block.write('11101001000', True)
+
